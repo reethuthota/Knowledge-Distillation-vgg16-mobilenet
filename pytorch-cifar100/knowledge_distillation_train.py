bf94d35f274fbce5aed8d746c2e9db8db9c30ecb
reethuthota <reethu.thota@gmail.com>
2024-05-06 19:19:08 +0530

    Create knowledge_distillation_train.py

---
+++ pytorch-cifar100/knowledge_distillation_train.py
@@ -0,0 +1,207 @@
+""" perform knowledge distillation using pytorch """
+
+import os
+import sys
+import argparse
+import time
+from datetime import datetime
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+
+from torch.utils.data import DataLoader
+
+from conf import settings
+from utils import get_network, get_training_dataloader, get_test_dataloader, WarmUpLR, \
+    most_recent_folder, most_recent_weights, last_epoch, best_acc_weights
+    
+def train(teacher, student, epoch, train_loader, soft_target_loss_weight, ce_loss_weight, T):
+    
+    start = time.time()
+    print("Training epoch : {epoch}", epoch)
+    running_loss = 0.0
+    for inputs, labels in train_loader:
+        if args.gpu:
+            labels = labels.cuda()
+            inputs = inputs.cuda()
+            
+        optimizer.zero_grad()
+
+        # Forward pass with the teacher model - do not save gradients here as we do not change the teacher's weights
+        with torch.no_grad():
+            teacher_logits = teacher(inputs)
+
+        # Forward pass with the student model
+        student_logits = student(inputs)
+
+        #Soften the student logits by applying softmax first and log() second
+        soft_targets = nn.functional.softmax(teacher_logits / T, dim=-1)
+        soft_prob = nn.functional.log_softmax(student_logits / T, dim=-1)
+
+        # Calculate the soft targets loss. Scaled by T**2 as suggested by the authors of the paper "Distilling the knowledge in a neural network"
+        soft_targets_loss = torch.sum(soft_targets * (soft_targets.log() - soft_prob)) / soft_prob.size()[0] * (T**2)
+
+        # Calculate the true label loss
+        label_loss = ce_loss(student_logits, labels)
+
+        # Weighted sum of the two losses
+        loss = soft_target_loss_weight * soft_targets_loss + ce_loss_weight * label_loss
+
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    #print(f"Epoch {epoch+1}/{epochs}, Loss: {running_loss / len(train_loader)}")
+    finish = time.time()
+
+    print('epoch {} training time consumed: {:.2f}s'.format(epoch, finish - start))
+
+@torch.no_grad()
+def eval_training(epoch=0, tb=True):
+
+    start = time.time()
+    kd_student.eval()
+
+    test_loss = 0.0 # cost function error
+    correct = 0.0
+
+    for (images, labels) in cifar100_test_loader:
+
+        if args.gpu:
+            images = images.cuda()
+            labels = labels.cuda()
+
+        outputs = kd_student(images)
+        loss = loss_function(outputs, labels)
+
+        test_loss += loss.item()
+        _, preds = outputs.max(1)
+        correct += preds.eq(labels).sum()
+
+    finish = time.time()
+    if args.gpu:
+        print('GPU INFO.....')
+        print(torch.cuda.memory_summary(), end='')
+    print('Evaluating Network.....')
+    print('Test set: Epoch: {}, Average loss: {:.4f}, Accuracy: {:.4f}, Time consumed:{:.2f}s'.format(
+        epoch,
+        test_loss / len(cifar100_test_loader.dataset),
+        correct.float() / len(cifar100_test_loader.dataset),
+        finish - start
+    ))
+    print()
+
+    return correct.float() / len(cifar100_test_loader.dataset)
+
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-gpu', action='store_true', default=False, help='use gpu or not')
+    parser.add_argument('-b', type=int, default=128, help='batch size for dataloader')
+    parser.add_argument('-warm', type=int, default=1, help='warm up training phase')
+    parser.add_argument('-lr', type=float, default=0.1, help='initial learning rate')
+    parser.add_argument('-resume', action='store_true', default=False, help='resume training')
+    args = parser.parse_args()
+
+    cifar100_training_loader = get_training_dataloader(
+        settings.CIFAR100_TRAIN_MEAN,
+        settings.CIFAR100_TRAIN_STD,
+        num_workers=4,
+        batch_size=args.b,
+        shuffle=True
+    )
+    
+    cifar100_test_loader = get_test_dataloader(
+        settings.CIFAR100_TRAIN_MEAN,
+        settings.CIFAR100_TRAIN_STD,
+        num_workers=4,
+        batch_size=args.b,
+        shuffle=True
+    )
+    
+    args.net = 'vgg16'
+    teacher = get_network(args)
+    teacher.load_state_dict(torch.load('/runs/vgg16/vgg16-164-best.pth'))
+    teacher.eval()
+    
+    args.net = 'mobilenet'
+    kd_student = get_network(args)
+    kd_student.load_state_dict(torch.load('/runs/mobilenet/mobilenet-124-best.pth'))
+    kd_student.train()
+    
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(kd_student.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+    train_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=settings.MILESTONES, gamma=0.2) #learning rate decay
+    iter_per_epoch = len(cifar100_training_loader)
+    warmup_scheduler = WarmUpLR(optimizer, iter_per_epoch * args.warm)
+    
+    if args.resume:
+        recent_folder = most_recent_folder(os.path.join(settings.CHECKPOINT_PATH, 'knowledge-distillation'), fmt=settings.DATE_FORMAT)
+        if not recent_folder:
+            raise Exception('no recent folder were found')
+
+        checkpoint_path = os.path.join(settings.CHECKPOINT_PATH, 'knowledge-distillation', recent_folder)
+
+    else:
+        checkpoint_path = os.path.join(settings.CHECKPOINT_PATH, 'knowledge-distillation', settings.TIME_NOW)
+        
+    #create checkpoint folder to save model
+    if not os.path.exists(checkpoint_path):
+        os.makedirs(checkpoint_path)
+    checkpoint_path = os.path.join(checkpoint_path, 'knowledge-distillation-{epoch}-{type}.pth')
+    
+    best_acc = 0.0
+    if args.resume:
+        best_weights = best_acc_weights(os.path.join(settings.CHECKPOINT_PATH, 'knowledge-distillation', recent_folder))
+        if best_weights:
+            weights_path = os.path.join(settings.CHECKPOINT_PATH, 'knowledge-distillation', recent_folder, best_weights)
+            print('found best acc weights file:{}'.format(weights_path))
+            print('load best training file to test acc...')
+            kd_student.load_state_dict(torch.load(weights_path))
+            best_acc = eval_training(tb=False)
+            print('best acc is {:0.2f}'.format(best_acc))
+
+        recent_weights_file = most_recent_weights(os.path.join(settings.CHECKPOINT_PATH, 'knowledge-distillation', recent_folder))
+        if not recent_weights_file:
+            raise Exception('no recent weights file were found')
+        weights_path = os.path.join(settings.CHECKPOINT_PATH, 'knowledge-distillation', recent_folder, recent_weights_file)
+        print('loading weights file {} to resume training.....'.format(weights_path))
+        kd_student.load_state_dict(torch.load(weights_path))
+
+        resume_epoch = last_epoch(os.path.join(settings.CHECKPOINT_PATH, 'knowledge-distillation', recent_folder))
+    
+    ce_loss = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(kd_student.parameters(), lr=args.lr)
+
+    teacher.eval()  # Teacher set to evaluation mode
+    kd_student.train() # Student to train mode
+    
+    for epoch in range(1, settings.EPOCH + 1):
+        if epoch > args.warm:
+            train_scheduler.step(epoch)
+
+        if args.resume:
+            if epoch <= resume_epoch:
+                continue
+
+        train(teacher, kd_student, epoch, cifar100_training_loader, soft_target_loss_weight=0.25, ce_loss_weight=0.75, T=2)
+        acc = eval_training(epoch)
+
+        #start to save best performance model after learning rate decay to 0.01
+        if epoch > settings.MILESTONES[1] and best_acc < acc:
+            weights_path = checkpoint_path.format(net=args.net, epoch=epoch, type='best')
+            print('saving weights file to {}'.format(weights_path))
+            torch.save('knowledge-distillation'.state_dict(), weights_path)
+            best_acc = acc
+            continue
+
+        if not epoch % settings.SAVE_EPOCH:
+            weights_path = checkpoint_path.format(net=args.net, epoch=epoch, type='regular')
+            print('saving weights file to {}'.format(weights_path))
+            torch.save('knowledge-distillation'.state_dict(), weights_path)
